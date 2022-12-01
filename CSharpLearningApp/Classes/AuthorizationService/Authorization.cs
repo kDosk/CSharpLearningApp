3b3c45a9632712113ac31b149ee1fbc5853add38
kDosk <stuser.ls@gmail.com>
2022-12-01 14:01:49 +0500

    Corrections made

--- CSharpLearningApp/Classes/AuthorizationService/Authorization.cs
+++ CSharpLearningApp/Classes/AuthorizationService/Authorization.cs
@@ -16,15 +16,22 @@ namespace CSharpLearningApp.Classes.AuthorizationService
 		{
 			try
 			{
-				ApplicationContext.GetContext().Users.Add(new User
+				if (!IsLoginBusy(login))
 				{
-					Surname = surname,
-					Name = name,
-					Login = login,
-					Password = HashingPassword(password),
-				});
-				ApplicationContext.GetContext().SaveChanges();
-				return GetUser(login, password);
+					ApplicationContext.GetContext().Users.Add(new User
+					{
+						Surname = surname,
+						Name = name,
+						Login = login,
+						Password = HashingPassword(password),
+					});
+					ApplicationContext.GetContext().SaveChanges();
+					return GetUser(login, password); 
+				}
+				else
+				{
+					return null;
+				}
 			}
 			catch (Exception)
 			{
@@ -52,6 +59,21 @@ namespace CSharpLearningApp.Classes.AuthorizationService
 				return null;
 			}
 		}
+
+		private static bool IsLoginBusy(string login)
+		{
+			bool toggle = false;
+			foreach (var item in ApplicationContext.GetContext().Users)
+			{
+				if (item.Login == login)
+				{
+					toggle = true;
+					break;
+				}
+			}
+			return toggle;
+		}
+
 		#pragma warning disable SYSLIB0021
 		private static string HashingPassword(string password)
 		{
